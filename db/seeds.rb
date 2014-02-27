94a5010179d9bcc238299e39726956a2773f1c6f
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-27 11:35:44 -0500

    created seeds for photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Comment.create(content: "Hey everyone!")
-Comment.create(content: "Catzzzz rule!")
-Comment.create(content: "first!11!!!")
+# Comment.create(content: "Hey everyone!")
+# Comment.create(content: "Catzzzz rule!")
+# Comment.create(content: "first!11!!!")
+
+Photo.create(url: "http://weknowmemes.com/wp-content/uploads/2012/12/aristocat-meme-jenkins.jpg")
+Photo.create(url: "http://weknowmemes.com/wp-content/uploads/2012/12/jenkins-warm-up-the-laptop.jpg")
+Photo.create(url: "http://www.quickmeme.com/img/33/33fa95fa72f30f46a71aafae91dde31092075b037d5b3367bf9e7b70b18a9965.jpg")
